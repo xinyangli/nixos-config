2d995896c265b7e4b5511a0939d4cb179303dca9
xinyangli <lixinyang411@gmail.com>
2024-07-30 11:01:07 +0800

    feat(weilite): make immich public

---
+++ machines/massicot/kanidm-provision.nix
@@ -0,0 +1,78 @@
+{
+  enable = true;
+  autoRemove = true;
+  groups = {
+    forgejo-access = {
+      members = [ "xin" ];
+    };
+    gts-users = {
+      members = [ "xin" ];
+    };
+    ocis-users = {
+      members = [ "xin" ];
+    };
+    linux_users = {
+      members = [ "xin" ];
+    };
+    hedgedoc-users = {
+      members = [ "xin" ];
+    };
+    immich-users = {
+      members = [ "xin" "zhuo" ];
+    };
+  };
+  persons = {
+    xin = {
+      displayName = "Xinyang Li";
+      mailAddresses = [ "lixinyang411@gmail.com" ];
+    };
+    
+    zhuo = {
+      displayName = "Zhuo";
+      mailAddresses = [ "13681104320@163.com" ];
+    };
+  };
+  systems.oauth2 = {
+    forgejo = {
+      displayName = "ForgeJo";
+      originUrl = "https://git.xinyang.life/";
+      originLanding = "	https://git.xinyang.life/user/oauth2/kandim";
+      allowInsecureClientDisablePkce = true;
+      scopeMaps = {
+        forgejo-access = [ "openid" "email" "profile" "groups" ];
+      };
+    };
+    gts = {
+      displayName = "GoToSocial";
+      originUrl = "https://xinyang.life/";
+      allowInsecureClientDisablePkce = true;
+      scopeMaps = {
+        gts-users = [ "openid" "email" "profile" "groups" ];
+      };
+    };
+    owncloud = {
+      displayName = "ownCloud";
+      originUrl = "https://home.xinyang.life:9201/";
+      public = true;
+      scopeMaps = {
+        ocis-users = [ "openid" "email" "profile" ];
+      };
+    };
+    hedgedoc = {
+      displayName = "HedgeDoc";
+      originUrl = "https://docs.xinyang.life/";
+      allowInsecureClientDisablePkce = true;
+      scopeMaps = {
+        hedgedoc-users = [ "openid" "email" "profile" ];
+      };
+    };
+    immich-mobile = {
+      displayName = "Immich";
+      originUrl = "https://immich.xinyang.life:8000/api/oauth/mobile-redirect/";
+      allowInsecureClientDisablePkce = true;
+      scopeMaps = {
+        immich-users = [ "openid" "email" "profile" ];
+      };
+    };
+  };
+}
