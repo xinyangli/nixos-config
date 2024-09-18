74fe93e5d804ab2c6c5c8572a985431b383b5413
xinyangli <lixinyang411@gmail.com>
2024-09-18 11:04:02 +0800

    pkgs: add ocis

--- overlays/add-pkgs.nix
+++ overlays/add-pkgs.nix
@@ -1,1 +1,4 @@
-(final: prev: { oidc-agent = prev.callPackage ./pkgs/oidc-agent { }; })
+(final: prev: {
+  oidc-agent = prev.callPackage ./pkgs/oidc-agent { };
+  ocis = prev.callPackage ./pkgs/ocis { };
+})
