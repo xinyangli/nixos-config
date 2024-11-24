5b19d8a97e17331ef95aa7e2db1f4b147915c9d0
xinyangli <lixinyang411@gmail.com>
2024-11-25 00:02:58 +0800

    weilite: more media services

--- machines/weilite/services/media-download.nix
+++ machines/weilite/services/media-download.nix
@@ -1,6 +1,23 @@
+{ pkgs, ... }:
 {
   services.jackett = {
     enable = true;
+    package = pkgs.jackett.overrideAttrs {
+      src = pkgs.fetchFromGitHub {
+        owner = "jackett";
+        repo = "jackett";
+        rev = "v0.22.998";
+        hash = "sha256-CZvgDWxxIAOTkodgmFNuT3VDW6Ln4Mz+Ki7m91f0BgE=";
+      };
+    };
     openFirewall = false;
+  };
+
+  services.sonarr = {
+    enable = true;
+  };
+
+  services.radarr = {
+    enable = true;
   };
 }
