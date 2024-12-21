133e70967fcff465535442585e3ad664f4beb21a
xinyangli <lixinyang411@gmail.com>
2024-12-21 21:34:44 +0800

    ci: point branch 'deploy-comin' to successful evaluations

--- modules/nixos/common-settings/comin.nix
+++ modules/nixos/common-settings/comin.nix
@@ -23,7 +23,7 @@ in
         {
           name = "origin";
           url = "https://github.com/xinyangli/nixos-config.git";
-          branches.main.name = "deploy-comin-eval";
+          branches.main.name = "deploy-comin";
         }
       ];
       hostname = config.networking.hostName;
