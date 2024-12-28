b8536f580184f4043c2da2d3a427fdd348ceb88a
xinyangli <lixinyang411@gmail.com>
2024-12-28 10:13:04 +0800

    calcite: global ctrl overload with keyd

--- machines/calcite/configuration.nix
+++ machines/calcite/configuration.nix
@@ -152,9 +152,13 @@ in
   services.keyd = {
     enable = true;
     keyboards = {
-      defualt = {
-        id = [ "*" ];
-        capslock = "overload(control, esc)";
+      default = {
+        ids = [ "*" ];
+        settings = {
+          main = {
+            capslock = "overload(control, esc)";
+          };
+        };
       };
       "internal" = {
         ids = [ "0b05:1866" ];
