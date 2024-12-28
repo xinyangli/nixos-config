40bed6459d12e0152bcc80ef427df262abc3082b
xinyangli <lixinyang411@gmail.com>
2024-12-28 10:52:56 +0800

    Revert "nixos/comin: use unified testing branch"
    This reverts commit 1d106c3d0937e18706c199f537a900bb52d386b3.

--- modules/nixos/common-settings/comin.nix
+++ modules/nixos/common-settings/comin.nix
@@ -24,7 +24,6 @@ in
           name = "origin";
           url = "https://github.com/xinyangli/nixos-config.git";
           branches.main.name = "deploy-comin";
-          branches.testing.name = "deploy-comin";
         }
       ];
       hostname = config.networking.hostName;
