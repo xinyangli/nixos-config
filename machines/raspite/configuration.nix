effcdee7f9dd13700b196f0ab2eb2673c4229c34
xinyangli <lixinyang411@gmail.com>
2025-03-15 21:35:42 +0800

    raspite: restore workaround for kernel mod

--- machines/raspite/configuration.nix
+++ machines/raspite/configuration.nix
@@ -14,6 +14,13 @@
     comin.enable = true;
   };
 
+  nixpkgs.overlays = [
+    # Workaround https://github.com/NixOS/nixpkgs/issues/126755#issuecomment-869149243
+    (final: super: {
+      makeModulesClosure = x: super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
+
   environment.systemPackages = with pkgs; [
     git
     libraspberrypi
