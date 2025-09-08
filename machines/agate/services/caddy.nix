3907228e94c879ca567ecb4edae2ba7959c9a1fe
Xinyang Li <lixinyang411@gmail.com>
2025-09-08 15:14:14 +0800

    agate: fix build

--- machines/agate/services/caddy.nix
+++ machines/agate/services/caddy.nix
@@ -28,7 +28,7 @@
     plugins = [
       "github.com/caddy-dns/cloudflare@v0.2.1"
     ];
-    hash = "sha256-saKJatiBZ4775IV2C5JLOmZ4BwHKFtRZan94aS5pO90=";
+    hash = "sha256-AcWko5513hO8I0lvbCLqVbM1eWegAhoM0J0qXoWL/vI=";
   };
 
   systemd.services.caddy = {
