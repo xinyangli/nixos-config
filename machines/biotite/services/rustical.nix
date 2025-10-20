ae162036086ecc38b40586c906e0059ce34b1931
Xinyang Li <lixinyang411@gmail.com>
2025-10-20 18:39:12 +0300

    rustical: added

--- machines/biotite/services/rustical.nix
+++ machines/biotite/services/rustical.nix
@@ -1,15 +1,27 @@
 { config, lib, ... }:
+let
+  inherit (config.my-lib.settings) idpUrl rusticalUrl;
+in
 {
   services.rustical = {
     enable = true;
     settings = {
+      oidc = {
+        name = "Kanidm";
+        issuer = "https://${idpUrl}/oauth2/openid/rustical";
+        client_id = "rustical";
+        claim_userid = "preferred_username";
+        scopes = [
+          "openid"
+          "profile"
+          "groups"
+        ];
+        allow_sign_up = false;
+      };
     };
   };
 
-  services.caddy.virtualHosts."derper01.xiny.li:8443".extraConfig = ''
-    tls {
-      dns cloudflare {env.CF_API_TOKEN}
-    }
-    reverse_proxy 127.0.0.1:${toString config.services.tailscale.derper.port}
+  services.caddy.virtualHosts.${rusticalUrl}.extraConfig = ''
+    reverse_proxy 127.0.0.1:${toString config.services.rustical.settings.http.port}
   '';
 }
