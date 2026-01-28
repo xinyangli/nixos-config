85bb26b0e818e271e0c32754907daf9dd42e3fdf
Xinyang Li <lixinyang411@gmail.com>
2026-01-28 20:08:17 +0200

    fixup! feat(biotite,agate): use oidc for jellyfin

--- overlays/my-lib/settings.nix
+++ overlays/my-lib/settings.nix
@@ -15,7 +15,7 @@
     matrixAuthUrl = "https://matrix-auth.xiny.li";
     synapseDelegateUrl = "https://synapse.xiny.li";
     rusticalUrl = "https://calendar.xiny.li";
-    jellyfinUrl = "https://agate.coho-tet.ts.net:8096";
+    jellyfinUrl = "https://agate.coho-tet.ts.net:8920";
 
     transmissionExporterUrl = "agate.coho-tet.ts.net:19091";
     ocisUrl = "https://drive.xiny.li:8443";
