ad4dbb1e1643f640d56a1b5a45773fda94acb087
Xinyang Li <lixinyang411@gmail.com>
2026-02-01 19:27:51 +0200

    feat(thorite/ntfy): add user to ntfy

--- machines/thorite/ntfy.nix
+++ machines/thorite/ntfy.nix
@@ -11,6 +11,11 @@ in
       listen-unix = "/var/run/ntfy-sh/ntfy.sock";
       listen-unix-mode = 432; # octal 0660
       base-url = ntfyUrl;
+      auth-file = "/var/lib/ntfy-sh/user.db";
+      auth-default-access = "read-write";
+      auth-users = [
+        "xin:$2b$05$zNV7bqsMrAFDRbg3QvKF9OFtUmmf0MrZysbJevJaS.88Y6NXawkhS:admin"
+      ];
     };
   };
 
