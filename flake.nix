4563b088423e6c75fa22f523416d835e3dee5319
Xinyang Li <lixinyang411@gmail.com>
2026-04-29 14:32:10 +0800

    fix(simple-mail-server): branch master -> main

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    simple-nixos-mailserver.url = "gitlab:simple-nixos-mailserver/nixos-mailserver/master";
+    simple-nixos-mailserver.url = "gitlab:simple-nixos-mailserver/nixos-mailserver/main";
 
     home-manager = {
       # TODO: https://github.com/nix-community/home-manager/pull/8484
