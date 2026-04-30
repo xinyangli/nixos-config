76c5dfe5982e4e97e6c1deaf250fc700b637bc94
Xinyang Li <lixinyang411@gmail.com>
2026-05-01 00:35:34 +0800

    fix(agate/hydra): drop libpqxx FIXME override
    NixOS/nixpkgs#476278 was fixed by NixOS/nixpkgs#483381, so the bundled
    libpqxx works again — revert the local override

--- machines/agate/services/hydra.nix
+++ machines/agate/services/hydra.nix
@@ -8,39 +8,6 @@
   '';
   services.hydra = {
     enable = true;
-    package =
-      # FIXME: https://github.com/NixOS/nixpkgs/issues/476278
-      (pkgs.hydra.override ({
-        libpqxx = (pkgs.libpqxx.override { stdenv = pkgs.gcc14Stdenv; }).overrideAttrs (
-          _: prevAttrs: {
-            nativeBuildInputs = prevAttrs.nativeBuildInputs ++ [ pkgs.autoreconfHook ];
-            nativeCheckInputs = [
-              pkgs.postgresql
-              pkgs.postgresqlTestHook
-            ];
-            postPatch = prevAttrs.postPatch + ''
-              # Disable some tests that always fail -- unclear why.
-              substituteInPlace test/unit/test_stream_from.cxx \
-                --replace-fail "PQXX_REGISTER_TEST(test_stream_from_parses_awkward_strings);" ""
-              substituteInPlace test/unit/test_stream_query.cxx \
-                --replace-fail "PQXX_REGISTER_TEST(test_stream_parses_awkward_strings);" ""
-              # Disable linting step for tests, it tries to install packages with pip.
-              substituteInPlace Makefile.am \
-                --replace-fail "TESTS = tools/lint" ""
-              # Needed for autoreconfHook
-              patchShebangs tools/*.py
-            '';
-            doCheck = true;
-            enableParallelBuilding = true;
-            __structuredAttrs = true;
-          }
-        );
-      })).overrideAttrs
-        (
-          _: _: {
-            doCheck = false;
-          }
-        );
     hydraURL = "http://agate.coho-tet.ts.net:3000/";
     notificationSender = "hydra@localhost";
     buildMachinesFiles = [ ];
