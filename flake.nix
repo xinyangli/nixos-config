c967e7ae6b8566cf16552ba358c52e099180dac4
Xinyang Li <lixinyang411@gmail.com>
2026-05-17 09:42:08 +0800

    flake: drop internet-traffic-assignments input
    Removes the input and its module references on agate/la-00. The
    `measure-{dns,iperf-latency,iperf-throughput,research}.{service,timer}`
    units that this module shipped will be GC'd from la-00 on the next
    nixos-rebuild switch.

--- flake.nix
+++ flake.nix
@@ -4,11 +4,6 @@
     nixpkgs.url = "github:xinyangli/nixpkgs/deploy";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
 
-    internet-traffic-assignments = {
-      url = "git+https://git.xiny.li/xin/InternetTraffic";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     simple-nixos-mailserver.url = "gitlab:simple-nixos-mailserver/nixos-mailserver/main";
 
     home-manager = {
@@ -117,7 +112,6 @@
     {
       self,
       nixpkgs,
-      internet-traffic-assignments,
       home-manager,
       nixos-hardware,
       sops-nix,
@@ -188,7 +182,6 @@
         agate = [
           disko.nixosModules.disko
           ./machines/agate
-          internet-traffic-assignments.nixosModules.assignment2
         ];
         hafnon = [
           disko.nixosModules.disko
@@ -205,7 +198,6 @@
           disko.nixosModules.disko
           ./machines/dolomite/bandwagon.nix
           ./machines/dolomite/common.nix
-          internet-traffic-assignments.nixosModules.assignment2
         ];
         fra-00 = [
           disko.nixosModules.disko
