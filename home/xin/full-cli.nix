e95979ccfeea8f5d7af7675e70bd00819636b296
Cyrus Li <cyrusli@canva.com>
2026-05-20 16:35:05 +1000

    home/xin: support XIN_FLAKES_FLAKEPATH and XIN_FLAKES_HM_USERNAME env overrides
    Allow xin-x86_64-full-cli to be deployed on hosts where the username
    and/or flake checkout path differ from the defaults. Both reads use
    builtins.getEnv, so nix must be invoked with --impure.

--- home/xin/full-cli.nix
+++ home/xin/full-cli.nix
@@ -1,9 +1,17 @@
-{ config, ... }:
+{ config, lib, ... }:
+let
+  envUsername = builtins.getEnv "XIN_FLAKES_HM_USERNAME";
+in
 {
   imports = [
     ./cli/essential.nix
     ./cli/extra.nix
   ];
+
+  home = lib.mkIf (envUsername != "") {
+    username = envUsername;
+    homeDirectory = "/home/${envUsername}";
+  };
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
--- overlays/my-lib/flakepath.nix
+++ overlays/my-lib/flakepath.nix
@@ -1,3 +1,8 @@
 {
-  flakePath = config: "${config.home.homeDirectory}/repo/personal/nixos-config";
+  flakePath =
+    config:
+    let
+      envPath = builtins.getEnv "XIN_FLAKES_FLAKEPATH";
+    in
+    if envPath != "" then envPath else "${config.home.homeDirectory}/repo/personal/nixos-config";
 }
