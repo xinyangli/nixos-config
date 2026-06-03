ce43cbb7b6f2f8dfcbf91f64529fee84544d8a9a
Xinyang Li <lixinyang411@gmail.com>
2026-06-03 15:10:33 +0800

    auth: bump kanidmd

--- machines/biotite/services/kanidm.nix
+++ machines/biotite/services/kanidm.nix
@@ -23,7 +23,7 @@ in
   };
 
   services.kanidm = {
-    package = lib.mkForce pkgs.kanidmWithSecretProvisioning_1_9;
+    package = lib.mkForce pkgs.kanidmWithSecretProvisioning_1_10;
     server = {
       enable = true;
       settings = {
